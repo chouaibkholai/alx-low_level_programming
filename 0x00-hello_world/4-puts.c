31117c5af648df7689dca131f395818512bd58e8
chouaibkholai <chouaib.kholai@gmail.com>
2023-03-16 13:44:39 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

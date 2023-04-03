62e4d1c2d9ebeb765d4327af2ea2ac373a57b28a
chouaibkholai <chouaib.kholai@gmail.com>
2023-04-03 09:19:31 -0700

    Task 3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strspn - the entry point
+ * @s: input
+ * @accept: input
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; accept[y] != s[x]; y++)
+		{
+			if (accept[y] == '\0')
+				return (x);
+		}
+	}
+	return (x);
+}

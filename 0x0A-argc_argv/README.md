0d7c9f1439e1d422095a4d82c538fb248af16c6d
chouaibkholai <chouaib.kholai@gmail.com>
2023-04-07 07:50:21 -0700

    Task 0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is a Readme for '0x0A. C - argc, argv' project
---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: 1 success.
+ * On error, -1 is returned and is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

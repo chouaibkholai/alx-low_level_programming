737b33aec359c201a6f4f19623afa64e66e08cde
chouaibkholai <chouaib.kholai@gmail.com>
2023-04-17 09:03:04 -0700

    Task 0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: Who lets the Dogs out!!
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
